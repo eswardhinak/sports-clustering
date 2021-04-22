f80af148521b485fc660bf2aa20659d50673b8f2
eswardhinak <eswardhinak@gmail.com>
2021-04-21 19:20:46 -0700

    Pylinted cluster_sports. Updated readm.e '

--- cluster_sports.py
+++ cluster_sports.py
@@ -1,11 +1,16 @@
+"""
+    cluster_sports.py - clusters sports from sports_rankings.csv based on user
+    selected dimensions and number of clusters
+"""
+
 import csv
 import random
 import math
 from constants import ALL_DIMENSIONS
 
-def get_initial_means(sports_data, dimensions, num_clusters):
+def get_initial_means(sports_data, num_clusters):
     """
-       randomly chooses points from sports_data as initial means
+        chooses random points from sports_data as initial means
     """
     sports_data_keys = list(sports_data)
     initial_means = []
@@ -35,33 +40,30 @@ def calculate_means(sports_dimensions_data, clusters, num_dimensions):
         means.append(cluster_mean)
     return means
 
-def lloyds_algorithm(means, sports_dimensions_data, num_dimensions, num_clusters, max_iterations=100):  
-    clusters = [[] for i in range(num_clusters)]
+def lloyds_algorithm(means, sports_dimensions_data,
+                    num_clusters, max_iterations=100):
     clusters_changed = False
-    num_sports = len(sports_dimensions_data)
 
     sport_to_cluster = {}
     count = 0
 
     while True:
-        new_clusters = [[] for i in range(num_clusters)]
+        clusters = [[] for i in range(num_clusters)]
         for sport in sports_dimensions_data:
             closest_mean = get_closest_mean(sports_dimensions_data[sport], means)
             if (sport not in sport_to_cluster or sport_to_cluster[sport] != closest_mean):
                 clusters_changed = True
-            new_clusters[closest_mean].append(sport)
+            clusters[closest_mean].append(sport)
             sport_to_cluster[sport] = closest_mean
         if count == max_iterations or not clusters_changed:
-            return [new_clusters, means]
-
-        clusters = new_clusters 
-        means = calculate_means(sports_dimensions_data, new_clusters, len(dimensions))
+            return [clusters, means]
+        means = calculate_means(sports_dimensions_data, clusters, len(dimensions))
         count += 1
 
 
 def validate_dimensions(dimensions_input):
     if dimensions_input.lower() == 'all':
-        return (True, [i for i in range(10)], 'Success')
+        return (True, range(10), 'Success')
     dimensions_input = dimensions_input.split(',')
     dimensions_list = []
     try:
@@ -82,7 +84,7 @@ def prompt_dimensions():
     print("You can cluster sports based on these 10 dimensions:")
     print("----------------------------------------------------")
     for i in range(10):
-        print(f'{ALL_DIMENSIONS[i][1]} - {ALL_DIMENSIONS[i][0]} - ({i})') 
+        print(f'{ALL_DIMENSIONS[i][1]} - {ALL_DIMENSIONS[i][0]} - ({i})')
     print("----------------------------------------------------\n")
 
     valid = False
@@ -91,10 +93,12 @@ def prompt_dimensions():
     while not valid:
         if message:
             print(message)
-        dimensions_input = input("Enter a comma-separated list of the numbers for each dimension you want to cluster on (Example: 4, 7, 2, 3): ")
+        dimensions_input = input("Enter a comma-separated list of the numbers \
+for each dimension you want to cluster on (Example: 4, 7, 2, 3): ")
         valid, dimensions, message = validate_dimensions(dimensions_input)
 
-    dimension_names = list(set([ALL_DIMENSIONS[i][0] for i in dimensions]))
+    dimensions = set(dimensions)
+    dimension_names = [ALL_DIMENSIONS[dim][0] for dim in dimensions]
     print('Chosen dimensions: ' + ', '.join(dimension_names))
     return dimensions
 
@@ -113,13 +117,14 @@ def prompt_num_clusters():
     while not valid:
         if message:
             print(message)
-        num_clusters_input = input("How many clusters do you want to build? Enter integer value between 2 and 59: ")
+        num_clusters_input = input("How many clusters do you want to build? \
+Enter integer value between 2 and 59: ")
         valid, num_clusters, message = validate_num_clusters(num_clusters_input)
     return num_clusters
 
 def get_sports_data_from_csv():
-    f = open('sports_rankings.csv')
-    csv_reader = csv.reader(f, delimiter='\t')
+    sports_csv = open('sports_rankings.csv')
+    csv_reader = csv.reader(sports_csv, delimiter='\t')
     next(csv_reader)    #skip first line (header)
 
     sports_data = {}
@@ -139,17 +144,17 @@ dimensions = prompt_dimensions()
 num_clusters = prompt_num_clusters()
 all_sports_data = get_sports_data_from_csv()
 sports_dimensions_data = get_required_dimensions(all_sports_data, dimensions)
-initial_means = get_initial_means(sports_dimensions_data, dimensions, num_clusters)
-clusters, means = lloyds_algorithm(initial_means, sports_dimensions_data, len(dimensions), num_clusters, max_iterations=1000)
+initial_means = get_initial_means(sports_dimensions_data, num_clusters)
+clusters, means = lloyds_algorithm(initial_means, sports_dimensions_data,
+                    num_clusters, max_iterations=1000)
 
 # displaying results
 dimension_names = [ALL_DIMENSIONS[dim][0] for dim in dimensions]
 dimension_text = ', '.join(dimension_names)
 
-for i in range(len(clusters)):
+for i, cluster in enumerate(clusters):
     print("Cluster # " + str(i))
     print(dimension_text)
-    print([round(mean, 2) for mean in means[i]])
-    for sport in clusters[i]:
+    print(str([round(mean, 2) for mean in means[i]]) + " - out of 10")
+    for sport in cluster:
         print("\t" +  sport)
-
